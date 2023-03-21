2b86f14c08fb5ad51b29507703ff2ec043f67e34
GoranRadmanovic <goranradmanovic@gmail.com>
2023-03-21 05:09:00 +0100

    Cleaning up the project, setting up SASS and starting with the header section

---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>
+    <img alt="IPS logo" class="logo" src="@/assets/icons/ips-logo.svg" />
+  </header>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/sass/main.sass'
+
 
 const app = createApp(App)
 
